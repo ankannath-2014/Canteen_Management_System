05da81e97ceddd586ed54ddb03c7d58a5596e8c5
ankann <ankann@trainee.nrifintech.com>
2022-12-21 12:37:17 +0530

    project template

---
+++ src/test/java/com/canteen/canteenms/CanteenManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canteen.canteenms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
